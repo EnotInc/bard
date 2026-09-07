25b5339d7a761f77c7ba7c6fb713811d7f67c546
EMosov <egordevil123@gmail.com>
2026-09-08 00:05:12 +0300

    fix error with tab insert duplicates

--- internal/tiles/editor/mode_insert.go
+++ internal/tiles/editor/mode_insert.go
@@ -103,8 +103,6 @@ func (e *Editor) caseInsert(key rune) {
 			return
 		}
 
-		tab := insertTab(cfg.KeepTabs, cfg.TabStop)
-		buf.Lines[buf.Cursor.Line()].Data = append(buf.Lines[buf.Cursor.Line()].Data, tab...)
 		if !cfg.KeepTabs {
 			curLine := buf.Lines[buf.Cursor.Line()]
 			tab := text.CursorShiftCalculateAt(curLine.Data, buf.Cursor.Offset(), cfg.TabStop)
